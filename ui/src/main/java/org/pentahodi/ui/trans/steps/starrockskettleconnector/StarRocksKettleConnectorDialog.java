e97f0d33f294ef7479aae05f7a814a49858d7753
nzm798 <798736800@qq.com>
2023-07-21 00:01:46 +0800

    update the impl

---
+++ ui/src/main/java/org/pentahodi/ui/trans/steps/starrockskettleconnector/StarRocksKettleConnectorDialog.java
@@ -0,0 +1,10 @@
+package org.pentahodi.ui.trans.steps.starrockskettleconnector;
+
+import org.pentaho.di.trans.step.StepDialogInterface;
+import org.pentaho.di.ui.trans.step.BaseStepDialog;
+
+/**
+ * Dialog class for the StarRocks Kettle Connector step.
+ */
+public class StarRocksKettleConnectorDialog extends BaseStepDialog implements StepDialogInterface {
+}
