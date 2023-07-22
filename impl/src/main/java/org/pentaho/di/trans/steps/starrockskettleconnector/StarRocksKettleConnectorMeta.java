e1944e63d9ea7e5622d1680928f67c9e64251643
nzm798 <798736800@qq.com>
2023-07-22 19:23:29 +0800

    update the impl

--- impl/src/main/java/org/pentaho/di/trans/steps/starrockskettleconnector/StarRocksKettleConnectorMeta.java
+++ impl/src/main/java/org/pentaho/di/trans/steps/starrockskettleconnector/StarRocksKettleConnectorMeta.java
@@ -1,103 +1,131 @@
 package org.pentaho.di.trans.steps.starrockskettleconnector;
 
+import org.pentaho.di.core.Const;
 import org.pentaho.di.core.annotations.Step;
+import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleXMLException;
 import org.pentaho.di.core.injection.Injection;
 import org.pentaho.di.core.injection.InjectionSupported;
 import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.repository.ObjectId;
+import org.pentaho.di.repository.Repository;
 import org.pentaho.di.trans.step.BaseStepMeta;
-import org.pentaho.di.trans.step.StepMetaInterface;
 import org.pentaho.metastore.api.IMetaStore;
 import org.w3c.dom.Node;
 
+import java.lang.String;
 import java.util.Arrays;
 import java.util.List;
 
-@Step( id = "StarRocksKettleConnector", name = "BaseStep.TypeLongDesc.StarRocksKettleConnector",
+@Step(id = "StarRocksKettleConnector", name = "BaseStep.TypeLongDesc.StarRocksKettleConnector",
         description = "BaseStep.TypeTooltipDesc.StarRocksKettleConnector",
         categoryDescription = "i18n:org.pentaho.di.trans.step:BaseStep.Category.Bulk",
         image = "BLKMYSQL.svg",
         documentationUrl = "http://wiki.pentaho.com/display/EAI/StarRocks+Kettle+Connector",
-        i18nPackageName = "org.pentaho.di.trans.steps.starrockskettleconnector" )
-@InjectionSupported( localizationPrefix = "StarRocksKettleConnector.Injection.", groups = { "FIELDS" } )
+        i18nPackageName = "org.pentaho.di.trans.steps.starrockskettleconnector")
+@InjectionSupported(localizationPrefix = "StarRocksKettleConnector.Injection.", groups = {"FIELDS"})
 public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRocksMeta {
 
-    private static Class<?> PKG=StarRocksKettleConnectorMeta.class; // for i18n purposes, needed by Translator2!!
+    private static Class<?> PKG = StarRocksKettleConnectorMeta.class; // for i18n purposes, needed by Translator2!!
 
     private static final long KILO_BYTES_SCALE = 1024L;
     private static final long MEGA_BYTES_SCALE = KILO_BYTES_SCALE * KILO_BYTES_SCALE;
     private static final long GIGA_BYTES_SCALE = MEGA_BYTES_SCALE * KILO_BYTES_SCALE;
 
-    /** Url of the stream load, if you you don't specify the http/https prefix, the default http. like: `fe_ip1:http_port;http://fe_ip2:http_port;https://fe_nlb`. */
+    /**
+     * Url of the stream load, if you you don't specify the http/https prefix, the default http. like: `fe_ip1:http_port;http://fe_ip2:http_port;https://fe_nlb`.
+     */
     @Injection(name = "LOAD_URL")
     private List<String> loadurl;
 
-    /** Url of the jdbc like: `jdbc:mysql://fe_ip1:query_port,fe_ip2:query_port...`. */
+    /**
+     * Url of the jdbc like: `jdbc:mysql://fe_ip1:query_port,fe_ip2:query_port...`.
+     */
     @Injection(name = "JDBC_URL")
     private String jdbcurl;
 
-    /** Database name of the stream load. */
+    /**
+     * Database name of the stream load.
+     */
     @Injection(name = "DATABASE_NAME")
     private String databasename;
 
-    /** Table name of the stream load. */
+    /**
+     * Table name of the stream load.
+     */
     @Injection(name = "TABLE_NAME")
     private String tablename;
 
-    /** StarRocks user name. */
+    /**
+     * StarRocks user name.
+     */
     @Injection(name = "USER")
     private String user;
 
-    /** StarRocks user password. */
+    /**
+     * StarRocks user password.
+     */
     @Injection(name = "PASSWORD")
     private String password;
 
-    /** The format of the data to be loaded. The value can be CSV or JSON. */
+    /**
+     * The format of the data to be loaded. The value can be CSV or JSON.
+     */
     @Injection(name = "FORMAT")
     private String format;
 
-    /** The maximum size of data that can be loaded into StarRocks at a time. */
+    /**
+     * The maximum size of data that can be loaded into StarRocks at a time.
+     */
     @Injection(name = "MAXBYTES")
     private Long maxbytes;
 
-    /** The maximum number of rows that can be loaded into StarRocks at one time. */
+    /**
+     * The maximum number of rows that can be loaded into StarRocks at one time.
+     */
     @Injection(name = "MAXROWS")
     private Long maxrows;
 
-    /** Timeout period for connecting to the load-url. */
+    /**
+     * Timeout period for connecting to the load-url.
+     */
     @Injection(name = "CONNECT_TIMEOUT")
     private Long connecttimeout;
 
-    /** Stream Load timeout period, in seconds. */
+    /**
+     * Stream Load timeout period, in seconds.
+     */
     @Injection(name = "TIMEOUT")
     private Long timeout;
 
-    /** Field name of the target table */
-    @Injection( name = "FIELD_TABLE", group = "FIELDS" )
+    /**
+     * Field name of the target table
+     */
+    @Injection(name = "FIELD_TABLE", group = "FIELDS")
     private String[] fieldTable;
 
-    /** Field name in the stream */
-    @Injection( name = "FIELD_STREAM", group = "FIELDS" )
+    /**
+     * Field name in the stream
+     */
+    @Injection(name = "FIELD_STREAM", group = "FIELDS")
     private String[] fieldStream;
 
     /**
      * @param loadurl Url of the stream load.
      */
-    public void setLoadurl(List<String> loadurl){
-        this.loadurl=loadurl;
+    public void setLoadurl(List<String> loadurl) {
+        this.loadurl = loadurl;
     }
 
     /**
-     *
      * @return Return the load url.
      */
-    public List<String> getLoadurl(){
+    public List<String> getLoadurl() {
         return this.loadurl;
     }
 
     /**
-     *
      * @return Return the JDBC url.
      */
     public String getJdbcurl() {
@@ -107,12 +135,11 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     /**
      * @param jdbcurl Url of the jdbc.
      */
-    public void setJdbcurl(String jdbcurl){
-        this.jdbcurl=jdbcurl;
+    public void setJdbcurl(String jdbcurl) {
+        this.jdbcurl = jdbcurl;
     }
 
     /**
-     *
      * @return Return the data base name.
      */
     public String getDatabasename() {
@@ -120,7 +147,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param databasename The target data base name.
      */
     public void setDatabasename(String databasename) {
@@ -128,7 +154,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Return the table name.
      */
     public String getTablename() {
@@ -136,7 +161,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param tablename The target table name.
      */
     public void setTablename(String tablename) {
@@ -144,7 +168,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Return the StarRocks user.
      */
     public String getUser() {
@@ -152,7 +175,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param user The StarRocks user.
      */
     public void setUser(String user) {
@@ -160,7 +182,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Returns the password for the user.
      */
     public String getPassword() {
@@ -168,7 +189,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param password The password for the user.
      */
     public void setPassword(String password) {
@@ -176,7 +196,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Returns the imported data format.
      */
     public String getFormat() {
@@ -184,7 +203,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param format The imported data format
      */
     public void setFormat(String format) {
@@ -192,7 +210,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Return the maximum size of data that can be loaded into StarRocks at a time.
      */
     public Long getMaxbytes() {
@@ -200,7 +217,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param maxbytes The maximum size of data that can be loaded into StarRocks at a time.
      */
     public void setMaxbytes(Long maxbytes) {
@@ -208,7 +224,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Return the maximum number of rows that can be loaded into StarRocks at one time.
      */
     public Long getMaxrows() {
@@ -216,7 +231,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param maxrows The maximum number of rows that can be loaded into StarRocks at one time.
      */
     public void setMaxrows(Long maxrows) {
@@ -224,7 +238,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Return timeout period for connecting to the load-url.
      */
     public Long getConnecttimeout() {
@@ -232,7 +245,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param connecttimeout Timeout period for connecting to the load-url.
      */
     public void setConnecttimeout(Long connecttimeout) {
@@ -240,7 +252,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @return Return Stream Load timeout period.
      */
     public Long getTimeout() {
@@ -248,7 +259,6 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     *
      * @param timeout Stream Load timeout period.
      */
     public void setTimeout(Long timeout) {
@@ -263,10 +273,9 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     * @param fieldTable
-     *          The fieldTable to set.
+     * @param fieldTable The fieldTable to set.
      */
-    public void setFieldTable( String[] fieldTable ) {
+    public void setFieldTable(String[] fieldTable) {
         this.fieldTable = fieldTable;
     }
 
@@ -278,77 +287,111 @@ public class StarRocksKettleConnectorMeta extends BaseStepMeta implements StarRo
     }
 
     /**
-     * @param fieldStream
-     *          The fieldStream to set.
+     * @param fieldStream The fieldStream to set.
      */
-    public void setFieldStream( String[] fieldStream ) {
+    public void setFieldStream(String[] fieldStream) {
         this.fieldStream = fieldStream;
     }
 
-    public void setDefault(){
-        loadurl=null;
-        jdbcurl=null;
-        databasename="";
-        tablename= BaseMessages.getString(PKG,"StarRocksKettleConnectorMeta.DefaultTableName");
-        user="root";
-        password="";
-        format="CSV";
-        maxbytes=90L * MEGA_BYTES_SCALE;
-        maxrows=500000L;
-        connecttimeout=1000L;
-        timeout=600L;
+    public void setDefault() {
+        fieldTable=null;
+        loadurl = null;
+        jdbcurl = null;
+        databasename = "";
+        tablename = BaseMessages.getString(PKG, "StarRocksKettleConnectorMeta.DefaultTableName");
+        user = "root";
+        password = "";
+        format = "CSV";
+        maxbytes = 90L * MEGA_BYTES_SCALE;
+        maxrows = 500000L;
+        connecttimeout = 1000L;
+        timeout = 600L;
     }
 
-    public void allocate( int nrvalues ){
-        fieldTable=new String[nrvalues];
-        fieldStream=new String[nrvalues];
+    public void allocate(int nrvalues) {
+        fieldTable = new String[nrvalues];
+        fieldStream = new String[nrvalues];
     }
 
     public Object clone() {
-        StarRocksKettleConnectorMeta retval=(StarRocksKettleConnectorMeta) super.clone();
-        int nrvalues= fieldTable.length;
+        StarRocksKettleConnectorMeta retval = (StarRocksKettleConnectorMeta) super.clone();
+        int nrvalues = fieldTable.length;
         retval.allocate(nrvalues);
-        System.arraycopy(fieldTable,0,retval.fieldTable,0,nrvalues);
-        System.arraycopy(fieldTable,0,retval.fieldTable,0,nrvalues);
+        System.arraycopy(fieldTable, 0, retval.fieldTable, 0, nrvalues);
+        System.arraycopy(fieldTable, 0, retval.fieldTable, 0, nrvalues);
 
         return retval;
     }
 
-    public void loadXML(Node stepnode, IMetaStore metaStore) throws KettleXMLException{
+    public void loadXML(Node stepnode, IMetaStore metaStore) throws KettleXMLException {
         readData(stepnode);
     }
-    private void readData(Node stepnode)throws KettleXMLException{
+
+    private void readData(Node stepnode) throws KettleXMLException {
         try {
-            String loadurl1= XMLHandler.getTagValue(stepnode,"loadurl");
-            loadurl= Arrays.asList(loadurl1.split(";"));
-            jdbcurl=XMLHandler.getTagValue(stepnode,"jdbcurl");
-            databasename=XMLHandler.getTagValue(stepnode,"databasename");
-            tablename=XMLHandler.getTagValue(stepnode,"tablename");
-            user=XMLHandler.getTagValue(stepnode,"user");
-            password=XMLHandler.getTagValue(stepnode,"password");
-            format=XMLHandler.getTagValue(stepnode,"format");
-            maxbytes=Long.valueOf(XMLHandler.getTagValue(stepnode,"maxbytes"));
-            maxrows=Long.valueOf(XMLHandler.getTagValue(stepnode,"maxrows"));
-            connecttimeout=Long.valueOf(XMLHandler.getTagValue(stepnode,"connecttimeout"));
-            timeout=Long.valueOf(XMLHandler.getTagValue(stepnode,"timeout"));
+            String loadurl1 = XMLHandler.getTagValue(stepnode, "loadurl");
+            loadurl = Arrays.asList(loadurl1.split(";"));
+            jdbcurl = XMLHandler.getTagValue(stepnode, "jdbcurl");
+            databasename = XMLHandler.getTagValue(stepnode, "databasename");
+            tablename = XMLHandler.getTagValue(stepnode, "tablename");
+            user = XMLHandler.getTagValue(stepnode, "user");
+            password = XMLHandler.getTagValue(stepnode, "password");
+            format = XMLHandler.getTagValue(stepnode, "format");
+            maxbytes = Long.valueOf(XMLHandler.getTagValue(stepnode, "maxbytes"));
+            maxrows = Long.valueOf(XMLHandler.getTagValue(stepnode, "maxrows"));
+            connecttimeout = Long.valueOf(XMLHandler.getTagValue(stepnode, "connecttimeout"));
+            timeout = Long.valueOf(XMLHandler.getTagValue(stepnode, "timeout"));
 
             // Field data mapping
-            int nrvalues=XMLHandler.countNodes(stepnode,"mapping");
+            int nrvalues = XMLHandler.countNodes(stepnode, "mapping");
             allocate(nrvalues);
 
-            for (int i=0;i<nrvalues;i++){
-                Node vnode=XMLHandler.getSubNodeByNr(stepnode,"mapping",i);
+            for (int i = 0; i < nrvalues; i++) {
+                Node vnode = XMLHandler.getSubNodeByNr(stepnode, "mapping", i);
 
-                fieldTable[i]=XMLHandler.getTagValue(vnode,"stream_name");
-                fieldStream[i]=XMLHandler.getTagValue(vnode,"field_name");
-                if (fieldStream[i]==null){
+                fieldTable[i] = XMLHandler.getTagValue(vnode, "stream_name");
+                fieldStream[i] = XMLHandler.getTagValue(vnode, "field_name");
+                if (fieldStream[i] == null) {
                     fieldStream[i] = fieldTable[i]; // default: the same name!
                 }
             }
 
-        }catch (Exception e){
-            throw new KettleXMLException(BaseMessages.getString(PKG,"StarRocksKettleConnectorMeta.Exception.UnableToReadStepInfoFromXML"),e);
+        } catch (Exception e) {
+            throw new KettleXMLException(BaseMessages.getString(PKG, "StarRocksKettleConnectorMeta.Exception.UnableToReadStepInfoFromXML"), e);
         }
 
+    }
+
+    public String getXML() {
+        StringBuilder retval = new StringBuilder(300);
+
+        String loadurl1= String.join(";",loadurl);
+        retval.append( "    " ).append( XMLHandler.addTagValue( "loadurl", loadurl1 ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "jdbcurl", jdbcurl ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "databasename", databasename ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "tablename", tablename ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "user", user ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "password", password ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "format", format ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "maxbytes", maxbytes ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "maxrows", maxrows ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "connecttimeout", connecttimeout ) );
+        retval.append( "    " ).append( XMLHandler.addTagValue( "timeout", timeout ) );
+
+        for ( int i = 0; i < fieldTable.length; i++ ) {
+            retval.append( "        " ).append( XMLHandler.addTagValue( "stream_name", fieldTable[i] ) );
+            retval.append( "        " ).append( XMLHandler.addTagValue( "field_name", fieldStream[i] ) );
+            retval.append( "      </mapping>" ).append( Const.CR );
+        }
+
+        return retval.toString();
+    }
+
+    public void readRep(Repository rep, IMetaStore metaStore, ObjectId id_step) throws KettleException{
+        try {
+
+        }catch (Exception e){
+            throw new KettleException(BaseMessages.getString(PKG,"StarRocksKettleConnectorMeta.Exception.UnexpectedErrorReadingStepInfoFromRepository"));
+        }
     }
 }
