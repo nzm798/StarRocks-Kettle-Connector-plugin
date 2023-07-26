60a165112882f85c4985a9d88663483f7ef51576
nzm798 <798736800@qq.com>
2023-07-26 16:50:19 +0800

    upadte the check.

--- impl/src/main/java/org/pentaho/di/trans/steps/starrockskettleconnector/StarRocksKettleConnectorData.java
+++ impl/src/main/java/org/pentaho/di/trans/steps/starrockskettleconnector/StarRocksKettleConnectorData.java
@@ -1,6 +1,7 @@
 package org.pentaho.di.trans.steps.starrockskettleconnector;
 
 import com.starrocks.data.load.stream.v2.StreamLoadManagerV2;
+import org.pentaho.di.core.util.StreamLogger;
 import org.pentaho.di.trans.step.BaseStepData;
 import org.pentaho.di.trans.step.StepDataInterface;
 
@@ -11,6 +12,13 @@ public class StarRocksKettleConnectorData extends BaseStepData implements StepDa
 
     // Use the Stream Load method to load the number.
     private StreamLoadManagerV2 streamLoadManager;
+
+    public int[] keynrs; // nr of keylookup -value in row...
+
+    public StreamLogger errorLogger;
+    public StreamLogger outputLogger;
+
+
 
     public StarRocksKettleConnectorData(){
         super();
